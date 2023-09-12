94212b069017bda38a31011e03c68c0ee7b6eac1
Richard Chalk <barmybrit@gmail.com>
2023-09-12 19:09:05 +0200

    Add project files.

---
+++ SeedProducts/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SeedProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nu kallar vi på metoden som seeda några produkter
+            WriteDefaultProducts();
+        }
+
+        static void WriteDefaultProducts()
+        {
+            if (File.Exists("../../../MyProducts.txt")) return;
+            string text = "11 Jordgubbar 39,90 Styckpris\n" +
+                          "22 Nutella 19,90 Styckpris\n" +
+                          "33 Citron 4,90 Styckpris\n" +
+                          "44 Bananer 9,90 Kilopris\n" +
+                          "55 Grädde 15,90 Styckpris\n" +
+                          "66 Choklad 9,90 Styckpris\n" +
+                          "77 Apelsiner 29,90 Kilopris\n";
+            File.WriteAllText("../../../MyProducts.txt", text);
+
+            // Prova!
+            // Vad händer om jag ändrar till dena kod?
+            // Denna saknas - ../../../
+            
+            // if (File.Exists("MyProducts.txt")) return;
+            // File.WriteAllText("MyProducts.txt", text);
+        }
+    }
+}
